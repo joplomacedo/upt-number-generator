5836afd2db7f48db1818ebf3635c9f9939aa7489
joplomacedo <joplomacedo@gmail.com>
2025-01-03 20:45:41 +0000

    Adds favicons

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+	plugins: [
+		react(),
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
